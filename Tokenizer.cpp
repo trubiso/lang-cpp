9db3c1db441d22d01c9b017dcc98c05d281f3c80
Trubiso <trubiso@users.noreply.github.com>
2024-01-19 22:06:07 +0100

    number literals in tokenizer

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -6,6 +6,7 @@ std::optional<Token> Tokenizer::next() {
 	consume_whitespace();
 	if (!current().has_value()) return {};
 	if (std::isalpha(current().value())) return consume_identifier();
+	if (std::isdigit(current().value())) return consume_number_literal();
 	switch (current().value()) {
 	case '"':
 		return consume_wrapped_literal('"', Token::Kind::STRING_LITERAL);
@@ -57,15 +58,34 @@ std::optional<Token> Tokenizer::consume_identifier() noexcept {
 }
 
 std::optional<Token> Tokenizer::consume_number_literal() noexcept {
-	// TODO
-	return {};
+	// TODO: be stricter
+	size_t start = m_index;
+	while (is_index_valid() && (std::isdigit(current().value()) || current().value() == '_'))
+		advance();
+	if (is_index_valid()) {
+		switch (current().value()) {
+		case 'x':
+		case 'b':
+		case 'o':
+			advance();  // consume the base character
+			break;
+		case '.':
+			advance();  // consume the decimal point (0x, 0b, 0o are integer only)
+			break;
+		}
+	}
+	while (is_index_valid() && (std::isxdigit(current().value()) || current().value() == '_'))
+		advance();
+	std::ignore = consume_identifier();  // just in case it ends with a postfix
+	size_t end = m_index;
+	return Token{.kind = Token::Kind::NUMBER_LITERAL, .span = Span{.start = start, .end = end}};
 }
 
 std::optional<Token> Tokenizer::consume_wrapped_literal(char wrap, Token::Kind kind) noexcept {
 	size_t start = m_index;
 	advance();  // consume wrap character
 	while (is_index_valid() && current().value() != wrap) advance();
-	if (!is_index_valid()) return {};    // if we didn't reach the wrap character, there's no token
+	if (!is_index_valid()) return {};  // if we didn't reach the wrap character, there's no token
 	// TODO: log unterminated wrapped literal error
 	advance();                           // consume wrap character
 	std::ignore = consume_identifier();  // just in case it ends with a postfix
