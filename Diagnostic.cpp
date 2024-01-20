d03221f15234cbe9c561badbb247a20896b14d10
Trubiso <trubiso@users.noreply.github.com>
2024-01-20 02:07:14 +0100

    diagnostic moving things around and more or less finished

--- Diagnostic.cpp
+++ Diagnostic.cpp
@@ -6,6 +6,8 @@
 
 #include "Util.hpp"
 
+#define TAB_WIDTH 4
+
 Diagnostic &Diagnostic::add_label(Span span, std::string label) {
 	labels.push_back(Label(span, std::move(label)));
 	return *this;
@@ -39,7 +41,7 @@ std::tuple<size_t, size_t> loc(std::string const *code, size_t idx, bool look_fo
 	for (size_t i = 0; i <= idx; i++) {
 		if (code->at(i) == '\n') {
 			l++;
-			li = i;
+			li = i + 1;  // seems to fix some bugs
 		}
 	}
 	if (look_for_next) {
@@ -54,16 +56,33 @@ std::tuple<size_t, size_t> loc(std::string const *code, size_t idx, bool look_fo
 	return {l, li};
 }
 
-void print_preline_labels(std::vector<Diagnostic::Label> const &things, std::string const *code, size_t loc_pad, uint8_t color_fg) {
+void print_loc_line(size_t loc_pad, std::optional<size_t> loc_curr) {
+	out_fmt_color_fg(8);
+	if (!loc_curr.has_value())
+		std::cout << std::string(loc_pad, ' ');
+	else
+		std::cout << std::string(loc_pad - std::to_string(loc_curr.value()).size(), ' ')
+		          << loc_curr.value();
+	std::cout << " │ ";
+}
+
+void print_preline_labels(std::vector<Diagnostic::Label> const &things, std::string const *code,
+                          size_t loc_pad, uint8_t color_fg, bool requires_last_padding = true) {
 	size_t j;
 	for (Diagnostic::Label const &label : things) {
 		size_t start_fixed;
 		std::tie(std::ignore, start_fixed) = loc(code, label.span.start);
-		out_fmt_color_fg(8);
-		std::cout << std::string(loc_pad, ' ') << " │ " << std::string();
+
+		// print pointy things
+		print_loc_line(loc_pad, {});
 		out_fmt_color_fg(color_fg);
 		j = start_fixed;
 		while (j < label.span.start) {
+			if (code->at(j) == '\t') {
+				std::cout << std::string(TAB_WIDTH, ' ');
+				j += TAB_WIDTH;
+				continue;
+			}
 			std::cout << ' ';
 			j++;
 		}
@@ -73,88 +92,107 @@ void print_preline_labels(std::vector<Diagnostic::Label> const &things, std::str
 		}
 		std::cout << "\n";
 
-		out_fmt_color_fg(8);
-		std::cout << std::string(loc_pad, ' ') << " │ " << std::string();
+		// print labels
+		print_loc_line(loc_pad, {});
 		out_fmt_color_fg(color_fg);
 		j = start_fixed;
 		while (j < label.span.start) {
+			if (code->at(j) == '\t') {
+				std::cout << std::string(TAB_WIDTH, ' ');
+				j += TAB_WIDTH;
+				continue;
+			}
 			std::cout << ' ';
 			j++;
 		}
 		std::cout << label.label << "\n";
 
-		out_fmt_color_fg(8);
-		std::cout << std::string(loc_pad, ' ') << " │ " << std::string();
-		std::cout << "\n";
+		if (requires_last_padding) {
+			print_loc_line(loc_pad, {});
+			std::cout << "\n";
+		}
 	}
 }
 
-void Diagnostic::print(std::string const *code) const {
-	out_fmt_bold();
-	out_fmt_color_fg(diagnostic_color(*this));
-	std::cout << severity_name(severity) << ":";
+void print_labels(std::vector<Diagnostic::Label> const &labels, std::string const *code,
+                  uint8_t color_fg) {
+	size_t start_l = SIZE_MAX;
+	size_t end_l = 0;
+	std::unordered_map<size_t, std::vector<Diagnostic::Label>> preline_labels;
+	for (Diagnostic::Label const &label : labels) {
+		if (label.span.start < start_l) start_l = label.span.start;
+		if (label.span.end > end_l) end_l = label.span.end;
+		size_t loc_lab;
+		std::tie(loc_lab, std::ignore) = loc(code, label.span.end);
+		if (!preline_labels.contains(loc_lab + 1))
+			preline_labels.insert({loc_lab + 1, std::vector<Diagnostic::Label>{}});
+		preline_labels.at(loc_lab + 1).push_back(label);
+	}
 
+	auto [loc_start, start] = loc(code, start_l);
+	auto [loc_end, end] = loc(code, end_l - 1, true);
+	size_t loc_pad = std::to_string(loc_end).size();
+	size_t loc_curr = loc_start;
+
+	std::cout << "\n";
+	print_loc_line(loc_pad, {});
+	std::cout << "\n";
+
+	for (size_t i = start; i < end; i++) {
+		if (i == start || code->at(i) == '\n') {
+			if (i != start) std::cout << "\n";
+			if (code->at(i) == '\n') loc_curr++;
+			std::string loc_curr_str = std::to_string(loc_curr);
+			if (preline_labels.contains(loc_curr))
+				print_preline_labels(preline_labels.at(loc_curr), code, loc_pad, color_fg);
+			print_loc_line(loc_pad, loc_curr);
+		}
+
+		if (code->at(i) != '\n') {
+			if (code->at(i) == '\t') {
+				std::cout << std::string(TAB_WIDTH, ' ');
+				continue;
+			}
+			out_fmt_color_fg(15);
+			for (Diagnostic::Label const &label : labels)
+				if (i >= label.span.start && i < label.span.end) out_fmt_color_fg(color_fg);
+			std::cout << code->at(i);
+		}
+	}
+	if (preline_labels.contains(loc_end + 1)) {
+		std::cout << "\n";
+		print_preline_labels(preline_labels.at(loc_end + 1), code, loc_pad, color_fg, false);
+	}
+
+	print_loc_line(loc_pad, {});
+	std::cout << "\n\n";
+}
+
+void Diagnostic::print(std::string const *code) const {
+	uint8_t color_fg = diagnostic_color(*this);
+
+	// Severity
+	out_fmt_bold();
+	out_fmt_color_fg(color_fg);
+	std::cout << severity_name(severity) << ":";
 	out_fmt_reset();
 	std::cout << " ";
 
+	// Title
 	out_fmt_bold();
 	std::cout << title << "\n";
 	out_fmt_reset();
 
+	// Subtitle
 	if (subtitle.has_value()) {
 		out_fmt_italic();
 		std::cout << subtitle.value() << "\n";
 		out_fmt_reset();
 	}
 
+	// Labels
 	if (labels.size() > 0) {
-		std::cout << "\n";
-		size_t start_l = SIZE_MAX;
-		size_t end_l = 0;
-		std::unordered_map<size_t, std::vector<Label>> preline_labels;
-		for (Label const &label : labels) {
-			if (label.span.start < start_l) start_l = label.span.start;
-			if (label.span.end > end_l) end_l = label.span.end;
-			size_t loc_lab;
-			std::tie(loc_lab, std::ignore) = loc(code, label.span.end);
-			if (!preline_labels.contains(loc_lab + 1))
-				preline_labels.insert({loc_lab + 1, std::vector<Label>{}});
-			preline_labels.at(loc_lab + 1).push_back(label);
-		}
-
-		auto [loc_start, start] = loc(code, start_l);
-		auto [loc_end, end] = loc(code, end_l - 1, true);
-		size_t loc_pad = std::to_string(loc_end).size();
-		size_t loc_curr = loc_start;
-
-		for (size_t i = start; i < end; i++) {
-			if (i == start || code->at(i) == '\n') {
-				if (i != start) std::cout << "\n";
-				if (code->at(i) == '\n') loc_curr++;
-				std::string loc_curr_str = std::to_string(loc_curr);
-				if (preline_labels.contains(loc_curr)) {
-					// TODO: sort ltr
-					print_preline_labels(preline_labels.at(loc_curr), code, loc_pad, diagnostic_color(*this));
-				}
-				out_fmt_color_fg(8);
-				std::cout << std::string(loc_pad - loc_curr_str.size(), ' ') << loc_curr << " │ ";
-			}
-			
-			if (code->at(i) != '\n') {
-				out_fmt_color_fg(15);
-				for (Label const &label : labels)
-					if (i >= label.span.start && i < label.span.end)
-						out_fmt_color_fg(diagnostic_color(*this));
-				std::cout << code->at(i);
-			}
-		}
-		if (preline_labels.contains(loc_end + 1)) {
-			std::cout << "\n";
-			// TODO: why is this off by 1
-			print_preline_labels(preline_labels.at(loc_end + 1), code, loc_pad, diagnostic_color(*this));
-		}
-
-		std::cout << "\n";
+		print_labels(labels, code, color_fg);
 	}
 
 	std::cout << std::endl;
