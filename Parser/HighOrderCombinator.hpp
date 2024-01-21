46cee619cea7b078e72a468c123a5f50f90d3b22
trubiso <33161454+trubiso@users.noreply.github.com>
2024-01-21 20:49:00 +0100

    angled

--- Parser/HighOrderCombinator.hpp
+++ Parser/HighOrderCombinator.hpp
@@ -9,6 +9,11 @@ inline Parser<Token, ParserError> wrapped(Parser<Token, ParserError> const &pars
 	return token_punctuation(l) >> parser << token_punctuation(r);
 }
 
+inline Parser<Token, ParserError> wrapped(Parser<Token, ParserError> const &parser,
+                                          Token::Operator l, Token::Operator r) {
+	return token_operator(l) >> parser << token_operator(r);
+}
+
 inline Parser<Token, ParserError> parenthesized(Parser<Token, ParserError> const &parser) {
 	return wrapped(parser, Token::Punctuation::L_PAREN, Token::Punctuation::R_PAREN);
 }
@@ -19,6 +24,10 @@ inline Parser<Token, ParserError> bracketed(Parser<Token, ParserError> const &pa
 
 inline Parser<Token, ParserError> braced(Parser<Token, ParserError> const &parser) {
 	return wrapped(parser, Token::Punctuation::L_BRACE, Token::Punctuation::R_BRACE);
+}
+
+inline Parser<Token, ParserError> angled(Parser<Token, ParserError> const &parser) {
+	return wrapped(parser, Token::Operator::LT, Token::Operator::GT);
 }
 
 };  // namespace Parser
