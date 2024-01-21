5e42078027b1ce4a60c5438b6033356384403d2e
trubiso <33161454+trubiso@users.noreply.github.com>
2024-01-21 15:32:58 +0100

    added more primitives

--- Parser/Primitive.cpp
+++ Parser/Primitive.cpp
@@ -6,7 +6,7 @@ template <typename T, typename E> Parser<T, E> constant(T const &value) {
 	return [=](Stream<Token> &) { return value; };
 }
 
-Parser<Token, ParserError> satisfy(std::function<bool(Token)> check) {
+Parser<Token, ParserError> satisfy(std::function<bool(Token const &)> check) {
 	return [=](Stream<Token> &input) -> Result<Token, ParserError> {
 		Token const &value = input.peek();
 		if (check(value)) {
@@ -20,16 +20,36 @@ Parser<Token, ParserError> satisfy(std::function<bool(Token)> check) {
 }
 
 Parser<Token, ParserError> token_kind(Token::Kind kind) {
-	return [=](Stream<Token> &input) -> Result<Token, ParserError> {
-		Token const &value = input.peek();
-		if (value.kind == kind) {
-			input.ignore();
-			return value;
-		} else {
-			// TODO: fix
-			return ParserError{};
-		}
-	};
+	return satisfy([=](Token const &token) { return token.kind == kind; });
+}
+
+Parser<Token, ParserError> token_keyword(Token::Keyword keyword) {
+	std::string desired;
+	switch (keyword) {
+	case Token::Keyword::LET:
+		desired = "let";
+	default:
+		throw "???";
+	}
+	return satisfy([=](Token const &token) {
+		if (token.kind != Token::Kind::IDENTIFIER) return false;
+		std::string const &value = std::get<std::string>(token.value);
+		return value == desired;
+	});
+}
+
+Parser<Token, ParserError> token_operator(Token::Operator operator_) {
+	return satisfy([=](Token const &token) {
+		if (token.kind != Token::Kind::OPERATOR) return false;
+		return std::get<Token::Operator>(token.value) == operator_;
+	});
+}
+
+Parser<Token, ParserError> token_punctuation(Token::Punctuation punctuation) {
+	return satisfy([=](Token const &token) {
+		if (token.kind != Token::Kind::PUNCTUATION) return false;
+		return std::get<Token::Punctuation>(token.value) == punctuation;
+	});
 }
 
 };  // namespace Parser
--- Parser/Primitive.hpp
+++ Parser/Primitive.hpp
@@ -5,8 +5,12 @@ namespace Parser {
 
 template <typename T, typename E> Parser<T, E> constant(T const &value);
 
-Parser<Token, ParserError> satisfy(std::function<bool(Token)> check);
+Parser<Token, ParserError> satisfy(std::function<bool(Token const &)> check);
 
 Parser<Token, ParserError> token_kind(Token::Kind kind);
+inline Parser<Token, ParserError> token_identifier() { return token_kind(Token::Kind::IDENTIFIER); }
+Parser<Token, ParserError> token_keyword(Token::Keyword keyword);
+Parser<Token, ParserError> token_operator(Token::Operator operator_);
+Parser<Token, ParserError> token_punctuation(Token::Punctuation punctuation);
 
 };  // namespace Parser
